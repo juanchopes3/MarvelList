7e22a91f68220113bf4d0772903979994f7426e2
juanchopes3 <107882205+juanchopes3@users.noreply.github.com>
2022-06-20 18:13:24 -0500

    add view and cell

---
+++ MarvelList/MarvelList/Main/View/Cells/MainCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MainCollectionViewCell.swift
+//  MarvelList
+//
+//  Created by Juan  Martinez on 20/06/22.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- MarvelList/MarvelList/Main/View/MainModuleViewController.swift
+++ MarvelList/MarvelList/Main/View/MainModuleViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 class MainModuleViewController: UIViewController {
 
 	var presenterModule: MainModulePresenterProtocol?
-
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var itemsCollection: UICollectionView!
+    
 	override func viewDidLoad() {
         super.viewDidLoad()
+        customizeUI()
+        
+    }
+    
+    private func customizeUI() {
+        itemsCollection.register(UINib(nibName: "MainCollectionViewCell", bundle: .main),
+                                forCellWithReuseIdentifier: "MainCollectionViewCell")
     }
 
 }
@@ -21,7 +31,18 @@ extension MainModuleViewController: MainModuleViewProtocol  {
     
 }
 
-extension MainModuleViewController {
+extension MainModuleViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCollectionViewCell", for: indexPath) as? MainCollectionViewCell
+
+        return cell!
+    }
+    
     
 }
 
